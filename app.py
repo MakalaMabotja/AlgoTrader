414aa01f2ed9f6140e1669e34fea43fac7631492
MakalaMabotja <138364127+MakalaMabotja@users.noreply.github.com>
2024-08-07 09:35:34 +0200

    Refactored app.py
    Modularized trading logic and signal generation and imported into the main app file

---
+++ app.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import yfinance as yf
+import pandas as pd
+
+from indicators.technical_indicators import Indicator
+from indicators.signals import Signals
+
+from models.data import fetch_data
+from models.visualizer import plot_candlestick, plot_ema, plot_levels, plot_rsi, plot_trades, plot_balance
+
+# Load data
+
+# Main script
+if __name__ == "__main__":
+    # Step 1: Load data
+    data = fetch_data()
+
+    # Step 2: Calculate technical indicators
+    data = Indicator(data).data.reset_index()
+
+        # Streamlit UI
+    st.title('Algorithmic Trading Dashboard')
+
+    # st.subheader('Candlestick Chart')
+    st.plotly_chart(plot_candlestick(data))
+
+    # st.subheader('Exponential Moving Averages')
+    st.plotly_chart(plot_ema(data))
+
+    # st.subheader('Support and Resistance Levels')
+    st.plotly_chart(plot_levels(data))
+
+    # st.subheader('Relative Strength Index')
+    st.plotly_chart(plot_rsi(data))
+
+    # st.subheader('Trades')
+    # st.plotly_chart(plot_trades(data, trades))
+
+    # st.subheader('Account Balance Over Time')
+
